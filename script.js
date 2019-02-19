d49f6dd92941820c1dfe7e6a3ffed2a8c1060b62
SaulB13 <46004728+SaulB13@users.noreply.github.com>
2019-02-19 23:34:57 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const dropdownButton = document.querySelector('.dropdown-button');
+const dropdown = document.querySelector('.dropdown');
+const scrollToTop = document.querySelector('.scroll-totop');
+
+dropdownButton.addEventListener('click', function(event) {
+	if (dropdown.style.display === 'none') {
+		dropdown.style.display = 'block';
+	} else {
+		dropdown.style.display = 'none';
+	}		
+});
+
+window.addEventListener('scroll', function(event) {
+	if (window.scrollY > 60) {
+		scrollToTop.style.display = 'block';
+	} else {
+		scrollToTop.style.display = 'none';
+	}
+});
+
+
+scrollToTop.addEventListener('click', function(event) {
+	window.scrollTo(0,0);
+});
